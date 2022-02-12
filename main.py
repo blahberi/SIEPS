578ac518263118d55afdb6c94e1aacecc3889f21
blahberi <along.blabri@gmail.com>
2022-02-12 13:53:18 +0200

    added a base64 option to the encoding

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from steganography.lsb import LSB
+
+LSB.encode("uAtbK6WZV00yJ3lnXpeEZGr+u6YcRBgW9oYLwrXHXg4QNeapoOD3FdMB8rJK5+wz", "unknown.png", "base64")
+print(LSB.decode("output.png", "base64"))
